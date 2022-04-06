6e7a7245ac7b4f85aceeaa46bbfc59f40a1dc302
Yuri Vinogradov <vvuri1978@gmail.com>
2022-04-07 02:01:16 +0300

    Start read new book

--- cmd/expgin/models/user.go
+++ cmd/expgin/models/user.go
@@ -1,9 +1,11 @@
 package models
 
 import (
+	"context"
 	"fmt"
 	"github.com/gofrs/uuid"
 	"github.com/jackc/pgx/v4"
+	"gopkg.in/h2non/gentleman.v2/plugins/query"
 	"strings"
 	"time"
 )
@@ -19,18 +21,27 @@ type User struct {
 }
 
 func (u *User) Register(conn *pgx.Conn) error {
+	// validate password
 	if len(u.Password)<6 {
 		return fmt.Errorf("Password must be at least 6 characters long.")
 	}
-
 	if u.Password != u.PasswordConfirm {
 		return fmt.Errorf("Password do not match.")
 	}
-
 	if len(u.Email) < 4 {
 		return fmt.Errorf("Incorrect email")
 	}
 
+	// validate email
 	u.Email = strings.ToLower(u.Email)
+	row := conn.QueryRow(context.Background(), "SELECT id from user_account WHERE email=$1", u.Email)
+	userLookup := User{}
+	err := row.Scan(&userLookup)
+	if err != pgx.ErrNoRows {
+		fmt.Println("Found user " + userLookup.Email)
+		return fmt.Errorf("User with this email alredy exists")
+	}
 
+	// create Hash password
+	pwdHash, err := bcrypt
 }
